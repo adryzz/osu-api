f9b9a707632e5288a4b8406cd7d86a97f0816c24
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-01-04 14:35:57 +0100

    added beatmap set

--- osu-api/OsuAPI.cs
+++ osu-api/OsuAPI.cs
@@ -4,5 +4,6 @@ namespace OsuAPI
 {
     public class OsuAPI
     {
+
     }
 }
---
+++ osu-api/Types/BeatmapsetCompact.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsuAPI.Types
+{
+    [Serializable()]
+    public struct BeatmapsetCompact
+    {
+        string Artist;
+        string ArtistUnicode;
+        BeatmapCovers Covers;
+        string Creator;
+        long FvouriteCount;
+        long Id;
+        long PlayCount;
+        string PreviewURL;
+        string Source;
+        RankStatus Status;
+        string Title;
+        string TitleUnicode;
+        long UserId;
+        string Video;
+        [Serializable()]
+        public enum RankStatus : int
+        {
+            Graveyard = -2,
+            WIP = -1,
+            Pending = 0,
+            Ranked = 1,
+            Approved = 2,
+            Qualified = 3,
+            Loved = 4
+        }
+
+        [Serializable()]
+        public struct BeatmapCovers
+        {
+            public string Cover;
+            public string Cover2x;
+            public string Card;
+            public string Card2x;
+            public string List;
+            public string List2x;
+            public string SlimCover;
+            public string SlimCover2x;
+        }
+    }
+}
